13564a62cdf207cc400c574b52b5ce14b4af9abe
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-11-19 02:31:34 +0300

    Initial Commit

---
+++ step-app/step_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  step_appApp.swift
+//  step-app
+//
+//  Created by Danil Peregorodiev on 19.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct step_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
