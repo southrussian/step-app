91c5af3d532047e647fd6467783829d6d4a95810
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-11-21 23:48:51 +0300

    Created Activity card view file

---
+++ step-app/Views/ActivityCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityCardView.swift
+//  step-app
+//
+//  Created by Danil Peregorodiev on 21.11.2023.
+//
+
+import SwiftUI
+
+struct ActivityCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityCardView()
+}
