0a77de07872ab04887b489fbcda545ea0e5eaab5
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-11-22 23:40:19 +0300

    Minor change

--- step-app/Views/ActivityCardView.swift
+++ step-app/Views/ActivityCardView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct ActivityCardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack {
+                Text("Steps")
+            }
+            .padding()
+            Spacer()
+            VStack {
+                Text("15")
+            }
+            .padding()
+            
+        }
     }
 }
 
